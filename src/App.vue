c9c88ffe47bb7787c3b0b17819ca526d5e497095
samuel-santos3112 <samuel.santos@gran.com>
2025-01-21 20:52:55 -0300

    feat:(wip): Adicionando componente de ConteudoPrincipal.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Banner />
+  <ConteudoPrincipal />
 </template>
 
 <script setup lang="ts">
 
 import Banner from '@/components/base/Banner.vue';
+import ConteudoPrincipal from '@/components/base/ConteudoPrincipal.vue';
 </script>
 
 <style scoped>
